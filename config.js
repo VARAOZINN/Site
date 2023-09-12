0ded1b8cb3f2a05ea4c12730835ad7fe8c581bec
FYZE 29'10 <73313428+VARAOZINN@users.noreply.github.com>
2023-09-11 21:37:48 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,54 @@
+module.exports = {
+    server: {
+        /*caso deixe como true, aparecera no topo do site o icone do servidor, nome e um botão escrito entrar, caso não queira apenas deixe como false */
+        ativado: true,
+        id: '1090800337239166976',
+        convite: 'https://discord.gg/HHHjUxXf'
+    },
+    bot: {
+        token: 'MTA2MjI0NTc4NjM4MzAyODI4NQ.GJAlhU.cjdjH8yHal_uEC_tbfFpzplddATsVzsVztCMVo'
+    },
+    pagina: {
+        /*particulas que saem do mouse ao mexer*/
+        particulas: 'https://cdn.discordapp.com/emojis/998607318893723798.png',
+        /* false deixa o avatar quadrado, true deixa o avatar redondo*/
+        avatar_redondo: true,
+        /* recomendo deixar vazio para ficar da cor_fundo, wallpaper fica mt feio */
+        background: '',
+        /*cor do background*/
+        cor_fundo: '#000001',
+        /*titulo que fica na aba do site*/
+        titulo_pagina: 'priv friends'
+        },
+
+    /*COLOQUE O ID DO USUÁRIO E O MES CORRESPONDENTE AO BOOSTER DELE, CASO ELE NÃO TENHA BOOST COLOQUE null, siga esse exemplo*/
+    usuarios: {
+      '393829826311749634': 'mes_24',
+      '775747755783487518': 'mes_24',
+      '265335759785951232': 'mes_9'
+
+    },
+    /* não mexa */
+    badges: {
+        nitro: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordnitro.svg',
+        PremiumEarlySupporter: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordearlysupporter.svg',
+        ActiveDeveloper: 'https://raw.githubusercontent.com/efeeozc/discord-badges/6c54af4b871af1e6daef9722f02257af18446129/svg/activedeveloper.svg',
+        HypeSquadOnlineHouse1: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbravery.svg',
+        HypeSquadOnlineHouse2: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbrilliance.svg',
+        HypeSquadOnlineHouse3: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbalance.svg',
+        VerifiedDeveloper: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordbotdev.svg',
+        novo_nick: 'https://github.com/mezotv/discord-badges/blob/main/assets/username.png?raw=true'
+    },
+    boost: {
+        mes_1: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost1.svg',
+        mes_2: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost2.svg',
+        mes_3: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost3.svg',
+        mes_6: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost4.svg',
+        mes_9: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost5.svg',
+        mes_12: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost6.svg',
+        mes_15: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost7.svg',
+        mes_18: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost8.svg',
+        mes_24: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost9.svg'
+    },
+    
+};
